d1846777084c470df13b495389100a146a5dcb10
fenghe <jxphxufh@gmail.com>
2021-08-26 02:26:44 +0800

    add joining_thread

--- thread_guard.h
+++ thread_guard.h
@@ -45,6 +45,76 @@ namespace thread_guard {
         }));
         std::cout << "using a and b inside the main thread\n";
     }
+
+
+    // Following is the C++17 proposal not taken
+
+    class joining_thread {
+        std::thread t;
+
+    public:
+        joining_thread() noexcept=default;
+        template<typename Callable, typename ... Args>
+        explicit joining_thread(Callable&& func, Args&& ... args)
+        {}
+
+        explicit joining_thread(std::thread t_) noexcept:
+            t(std::move(t_))
+        {}
+
+        joining_thread(joining_thread&& other) noexcept:
+            t(std::move(other.t))
+        {}
+
+        joining_thread& operator=(joining_thread& other) noexcept
+        {
+            if(joinable())
+                join();
+            t = std::move(other.t);
+            return *this;
+        }
+
+        ~joining_thread() noexcept
+        {
+            if(joinable())
+                join();
+        }
+
+        void swap(joining_thread& other) noexcept
+        {
+            t.swap(other.t);
+        }
+
+        std::thread::id get_id() const noexcept
+        {
+            return t.get_id();
+        }
+
+        bool joinable() const noexcept
+        {
+            return t.joinable();
+        }
+
+        void join()
+        {
+            return t.join();
+        }
+
+        void detach()
+        {
+            t.detach();
+        }
+
+        std::thread& as_thread() noexcept
+        {
+            return t;
+        }
+
+        const std::thread& as_thread() const noexcept
+        {
+            return t;
+        }
+    };
 }
 
 
